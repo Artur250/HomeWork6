d60073d4128eab0e518736932fa847f2c726f338
Artur250 <gameartur012@gmail.com>
2023-03-19 12:32:05 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,192 @@
+//1
+// const body = document.body 
+
+
+// const int1 = document.createElement('input') 
+// const p1 = document.createElement('p') 
+// p1.innerText = 'Введите ваше имя'
+
+// const int2 = document.createElement('input') 
+// const p2 = document.createElement('p') 
+// p2.innerText = 'Введите вашу фамилию'
+
+// const btn1 = document.createElement('button')
+// btn1.innerText = 'Отправить'
+
+
+// body.before(p1)
+// body.before(int1) 
+
+// body.prepend(int2)
+// body.prepend(p2)
+
+// body.after(btn1)
+ 
+//2
+// const body = document.body
+
+// const mainDiv = document.createElement('div')
+// mainDiv.classList.add('d')
+// mainDiv.style.border = '1px solid hsl(0deg 0% 100%))'
+// mainDiv.style.width = '50%'
+// mainDiv.style.padding = '1rem'
+
+
+
+// const btn1 = document.createElement('button')
+// btn1.innerText = 'Перейти к оплате'
+// btn1.style.backgroundColor = 'hsl(4.53deg 76.81% 72.94%)'
+// btn1.style.border = 'none'
+// btn1.style.padding = '18px 38px'
+// btn1.style.borderRadius = '10px'
+
+// mainDiv.prepend(btn1)
+
+
+
+// const firstSum = document.createElement('p')
+// firstSum.innerHTML = 'Ввелите сумму оплаты'
+// mainDiv.prepend(firstSum)
+
+// const sumInput = document.createElement('input')
+// sumInput.style.borderRadius = '5px'
+// sumInput.style.border = '1px solid gray'
+// sumInput.style.background = 'hsl(0deg 5.56% 85.88%)'
+// sumInput.style.width = '200px'
+// sumInput.style.height = '30px'
+// firstSum.after(sumInput)
+
+
+// const firstEmail = document.createElement('p')
+// firstEmail.innerHTML = 'Электронный адрес:'
+// mainDiv.prepend(firstEmail)
+
+// const emailInput = document.createElement('input')
+// emailInput.setAttribute('placeholder',"test@example.com")
+// emailInput.style.borderRadius = '5px'
+// emailInput.style.border = '1px solid gray'
+// emailInput.style.background = 'hsl(0deg 5.56% 85.88%)'
+// emailInput.style.width = '200px'
+// emailInput.style.height = '30px'
+// firstEmail.after(emailInput)
+
+// const firstNumber = document.createElement('p')
+// firstNumber.innerHTML = 'Телефон:'
+// mainDiv.prepend(firstNumber)
+
+// const numberInput = document.createElement('input')
+// numberInput.style.borderRadius = '5px'
+// numberInput.style.border = '1px solid gray'
+// numberInput.style.background = 'hsl(0deg 5.56% 85.88%)'
+// numberInput.style.width = '200px'
+// numberInput.style.height = '30px'
+// firstNumber.after(numberInput)
+
+// const secondNum = document.createElement('p')
+// secondNum.innerHTML = 'Введите номер заказа:'
+// mainDiv.prepend(secondNum)
+
+// const secondInput = document.createElement('input')
+// secondInput.setAttribute('placeholder','Оставьте сообщение здесь')
+// secondInput.style.borderRadius = '5px'
+// secondInput.style.border = '1px solid gray'
+// secondInput.style.background = 'hsl(0deg 5.56% 85.88%)'
+// secondInput.style.width = '200px'
+// secondInput.style.height = '30px'
+// secondNum.after(secondInput)
+
+// const secondName = document.createElement('p')
+// secondName.innerHTML = 'Фамилия Имя'
+// mainDiv.prepend(secondName)
+
+// const nameInput = document.createElement('input')
+// nameInput.style.borderRadius = '5px'
+// nameInput.style.border = '1px solid gray'
+// nameInput.style.background = 'hsl(0deg 5.56% 85.88%)'
+// nameInput.style.width = '200px'
+// nameInput.style.height = '30px'
+// secondName.after(nameInput)
+
+
+
+// body.before(mainDiv)
+
+
+//3
+// const body = document.body
+
+// const mainDiv = document.createElement('div')
+// mainDiv.classList.add('d')
+// mainDiv.style.border = '1px solid hsl(4.21deg 89.06% 74.9%)'
+// mainDiv.style.width = '50%'
+// mainDiv.style.padding = '1rem'
+
+
+
+// const btn1 = document.createElement('button')
+// btn1.innerText = 'Go to the payment'
+// btn1.style.backgroundColor = 'hsl(4.53deg 76.81% 72.94%)'
+// btn1.style.border = 'none'
+// btn1.style.padding = '18px 38px'
+// btn1.style.borderRadius = '10px'
+
+// mainDiv.prepend(btn1)
+
+
+
+// const firstSum = document.createElement('p')
+// firstSum.innerHTML = 'Sum:'
+// mainDiv.prepend(firstSum)
+
+// const sumInput = document.createElement('input')
+// sumInput.setAttribute('placeholder','sum')
+// sumInput.style.borderRadius = '5px'
+// sumInput.style.border = '1px solid gray'
+// sumInput.style.background = 'hsl(0deg 5.56% 85.88%)'
+// sumInput.style.width = '200px'
+// sumInput.style.height = '30px'
+// firstSum.after(sumInput)
+
+
+// const firstEmail = document.createElement('p')
+// firstEmail.innerHTML = 'Email:'
+// mainDiv.prepend(firstEmail)
+
+// const emailInput = document.createElement('input')
+// emailInput.setAttribute('placeholder','email')
+// emailInput.style.borderRadius = '5px'
+// emailInput.style.border = '1px solid gray'
+// emailInput.style.background = 'hsl(0deg 5.56% 85.88%)'
+// emailInput.style.width = '200px'
+// emailInput.style.height = '30px'
+// firstEmail.after(emailInput)
+
+// const firstNumber = document.createElement('p')
+// firstNumber.innerHTML = 'Number:'
+// mainDiv.prepend(firstNumber)
+
+// const numberInput = document.createElement('input')
+// numberInput.setAttribute('placeholder','number')
+// numberInput.style.borderRadius = '5px'
+// numberInput.style.border = '1px solid gray'
+// numberInput.style.background = 'hsl(0deg 5.56% 85.88%)'
+// numberInput.style.width = '200px'
+// numberInput.style.height = '30px'
+// firstNumber.after(numberInput)
+
+// const secondName = document.createElement('p')
+// secondName.innerHTML = 'Name:'
+// mainDiv.prepend(secondName)
+
+// const nameInput = document.createElement('input')
+// nameInput.setAttribute('placeholder','name')
+// nameInput.style.borderRadius = '5px'
+// nameInput.style.border = '1px solid gray'
+// nameInput.style.background = 'hsl(0deg 5.56% 85.88%)'
+// nameInput.style.width = '200px'
+// nameInput.style.height = '30px'
+// secondName.after(nameInput)
+
+
+
+// body.before(mainDiv)
